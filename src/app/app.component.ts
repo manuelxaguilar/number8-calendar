2b29675a4eda89319024c589592c6352c7c35e61
manuelxaguilar <manuelxaguilar@gmail.com>
2017-09-05 11:12:27 -0600

    Add onsubmit method & catch user values

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
+import moment from 'moment/src/moment'
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,6 +11,9 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class AppComponent implements OnInit {
   title = 'app';
   public form: FormGroup;
+  public startingDate: string = '';
+  public endingDate;
+  public countryCode: string = ''
 
   constructor(
     private fb:FormBuilder,
@@ -25,4 +30,17 @@ export class AppComponent implements OnInit {
       code: ['', [Validators.required]],
     });
   }
+
+  onSubmit() {
+    if (this.form.valid) {
+      this.startingDate = this.form.controls.date.value;
+      this.endingDate = moment(this.startingDate).add(this.form.controls.days.value, 'd');
+      this.countryCode = this.form.controls.code.value;
+    }
+    console.log('this.startingDate', this.startingDate);
+    console.log('this.endingDate', this.endingDate);
+    console.log('this.countryCode', this.countryCode);
+  }
+
+
 }
