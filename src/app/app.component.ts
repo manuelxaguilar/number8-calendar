563db1c7cba87cff99d9b966bd3a2a14b2fb875d
manuelxaguilar <manuelxaguilar@gmail.com>
2017-09-05 11:02:37 -0600

    Add form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+  public form: FormGroup;
+
+  constructor(
+    private fb:FormBuilder,
+  ) {}
+
+  ngOnInit() {
+    this.initForm();
+  }
+
+  initForm(card?: any) {
+    this.form = this.fb.group({
+      date: ['', [Validators.required]],
+      days: ['', [Validators.required]],
+      code: ['', [Validators.required]],
+    });
+  }
 }
